51663869bfd8d595fb0fe72365ba8c4cebf0dbe5
Saurabh <saurabh32yeole@gmail.com>
2026-06-08 10:48:13 +0530

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int[] arr = {1,0,8,0 ,4,5,0,6,7,8,0};
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int)(Math.random()*100);
+        }
         for(int a = 0; a < arr.length; a++){
             for(int b = 0; b < arr.length; b++){
                 if(arr[b] < arr[a]){
@@ -16,5 +19,6 @@ public class Main{
         for(int a = 0; a < arr.length; a++){
             System.out.print(arr[a] + " ");
         }
+
     }
 }
