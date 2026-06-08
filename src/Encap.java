adada9d56559023deb59bdf55e38a39cc4b36d65
Saurabh <saurabh32yeole@gmail.com>
2026-06-08 21:25:28 +0530

    Encapsulation

--- src/Encap.java
+++ src/Encap.java
@@ -9,16 +9,16 @@ class Human {
         return age;
     }
 
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
@@ -29,11 +29,13 @@ public class Encap {
         Human obj = new Human();
         System.out.print("Enter your age:");
         int k = s.nextInt();
-
         obj.setAge(k);
+        System.out.print("Enter your name:");
+        String str = s.next();
+        obj.setName(str);
 
-        System.out.print("Your age is: " + obj.getAge());
-
+        System.out.println("Your age is: " + obj.getAge());
+        System.out.println("Your name is: " + obj.getName());
         s.close();
     }
 }
