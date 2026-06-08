8400495e15569d01f6c7d840c92cdb53221cec2c
Saurabh <saurabh32yeole@gmail.com>
2026-06-08 11:29:38 +0530

    Enhance for Loop

---
+++ src/EforLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EforLoop {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n[] = {1,2,3,4,5,6,7};
+
+        for(int number : n){
+            System.out.println(number);
+        }
+    }
+}
