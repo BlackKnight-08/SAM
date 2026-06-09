549ceead2b8b07833c09ae67c8a47577f30f60ef
Saurabh <saurabh32yeole@gmail.com>
2026-06-09 18:40:22 +0530

    static keyword

---
+++ src/statickeyword.java
@@ -0,0 +1,34 @@
+// static variable to be accessed using class name
+class Mobile{
+    String brand;
+    int price;
+    static String name;    //static variable is common for all
+
+
+    public void show(){
+        System.out.println("Mobile brand is : " + brand + " and name is: " + name + " and price is: " + price);
+    }
+}
+
+
+
+public class statickeyword
+{
+    public static void main(String[] args)
+    {
+        Mobile obj = new Mobile();
+        obj.brand= "Apple";
+        obj.price=100;
+        Mobile.name="Phone";
+
+        Mobile obj1 = new Mobile();
+        obj1.brand="Apple";
+        obj1.price=100;
+        Mobile.name="Smartphone";
+
+        Mobile.name = "babaphone";
+
+        obj.show();
+        obj1.show();
+    }
+}
